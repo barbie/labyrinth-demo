cb96052fe1d3f10edc4027d9be545a05b1368b2f
Barbie <barbie@missbarbell.co.uk>
2014-12-14 11:11:15 +0000

    license update

--- lib/Labyrinth/Demo.pm
+++ lib/Labyrinth/Demo.pm
@@ -183,7 +183,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
   Copyright (C) 2002-2014 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
-  This module is free software; you can redistribute it and/or
+  This distribution is free software; you can redistribute it and/or
   modify it under the Artistic License 2.0.
 
 =cut
